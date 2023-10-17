cb484283e954196962549d0b6e6dc3c9109b0810
LufsX <lufsx@outlook.com>
2023-10-17 21:14:00 +0800

    add: wiki2txt tool

---
+++ wiki2txt.py
@@ -0,0 +1,26 @@
+import sys
+import datetime
+
+from gensim.corpora import WikiCorpus
+
+
+def wiki2txt(articles_data_path):
+    wiki_corpus = WikiCorpus(articles_data_path, dictionary={}, processes=16)
+
+    with open("articles_texts.txt", "w", encoding="utf-8", buffering=4096) as f:
+        num = 0
+        wait_to_write = []
+        for text in wiki_corpus.get_texts():
+            wait_to_write.append(" ".join(text) + "\n")
+            num += 1
+            if num % 10000 == 0:
+                f.writelines(wait_to_write)
+                wait_to_write.clear()
+                print(f"{datetime.datetime.now().strftime("%H:%M:%S.%f")} 处理了 {num} 个条目")
+    print("处理完成")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 " + sys.argv[0] + "wiki articles_data_path")
+        exit()
+    wiki2txt(sys.argv[1])
