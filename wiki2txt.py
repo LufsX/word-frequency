cb1a637b851f3189530538ab348b7eb8018044d6
LufsX <lufsx@outlook.com>
2023-10-20 23:45:17 +0800

    fix(wiki2txt): end of the file not to written

--- wiki2txt.py
+++ wiki2txt.py
@@ -5,7 +5,9 @@ from gensim.corpora import WikiCorpus
 
 
 def wiki2txt(articles_data_path, output_file_path):
-    print(f"{datetime.datetime.now().strftime('%H:%M:%S.%f')} 开始处理 {articles_data_path}")
+    print(
+        f"{datetime.datetime.now().strftime('%H:%M:%S.%f')} 开始处理 {articles_data_path}"
+    )
     wiki_corpus = WikiCorpus(articles_data_path, dictionary={})
 
     with open(output_file_path, "w", encoding="utf-8", buffering=4096) as f:
@@ -17,12 +19,20 @@ def wiki2txt(articles_data_path, output_file_path):
             if num % 10000 == 0:
                 f.writelines(wait_to_write)
                 wait_to_write.clear()
-                print(f"{datetime.datetime.now().strftime('%H:%M:%S.%f')} 处理了 {num} 个条目")
+                print(
+                    f"{datetime.datetime.now().strftime('%H:%M:%S.%f')} 处理了 {num} 个条目"
+                )
+        if wait_to_write:
+            f.writelines(wait_to_write)
+            wait_to_write.clear()
     print(f"{datetime.datetime.now().strftime('%H:%M:%S.%f')} 保存到 {output_file_path}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print(f"Usage: python3 {sys.argv[0]} path/to/wiki_articles_data path/to/output_file")
+        print(
+            f"Usage: python3 {sys.argv[0]} path/to/wiki_articles_data path/to/output_file"
+        )
         exit()
 
     input_file = sys.argv[1]
